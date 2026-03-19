edcde73abbf8647aa594a9e41dad6f1516d25e6a
koszys <forzadriftergod@gmail.com>
2026-03-19 02:36:41 -0400

    Footer with copyright and privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ export default function App() {
       }>
         <Route index element={<GenshinHome />} />
         <Route path="planner" element={<GenshinPlanner />} />
+        {/* <Route path="/privacy" element={<Privacy />} /> */}
       </Route>
 
     </Routes>
